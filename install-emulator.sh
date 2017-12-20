e4fbbdc2fcd92d42a82305ea03fc39d4f75d28e8
Taylor Newill <tanewill@microsoft.com>
2017-12-20 10:38:58 -0500

    quiet the wget's

--- install-emulator.sh
+++ install-emulator.sh
@@ -8,7 +8,7 @@ sudo yum install -y -q unzip wget
 sudo yum group install -y -q "Development Tools"
 mkdir -p /opt/vax_emulator/boot
 cd /opt/vax_emulator/
-wget https://azbenchmarkstorage.blob.core.windows.net/vax/vax_emulator.tgz
+wget -q https://azbenchmarkstorage.blob.core.windows.net/vax/vax_emulator.tgz
 tar -xzvf vax_emulator.tgz
 cp dua/dua0.vdisk boot/
 cp simhv390/VAX/ka655x.bin boot/
@@ -16,7 +16,7 @@ cd simhv390
 make
 cd ../boot
 ln -s /opt/vax_emulator/simhv390/BIN/vax vax
-wget https://raw.githubusercontent.com/tanewill/vaxemulator/master/vax.ini
+wget -q https://raw.githubusercontent.com/tanewill/vaxemulator/master/vax.ini
 sed -i "s/_USER/$USER/g" vax.ini
 chown -R $NEWUSER /opt/vax_emulator
 ln -s /opt/vax_emulator /home/$NEWUSER/vax_emulator
